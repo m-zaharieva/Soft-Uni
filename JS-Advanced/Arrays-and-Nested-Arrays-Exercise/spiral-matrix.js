a72998bdc408e974e05d9dd559da97151f3e1b66
Мирослава Захариева <miroslava.zaharieva@gmail.com>
2021-05-31 09:33:57 +0300

    edited file

--- JS-Advanced/Arrays-and-Nested-Arrays-Exercise/spiral-matrix.js
+++ JS-Advanced/Arrays-and-Nested-Arrays-Exercise/spiral-matrix.js
@@ -1,16 +1,47 @@
 function solve(x, y) {
     let matrix = [];
-    let currentNumber = 1; 
+    let currentNumber = 1;
 
     for (let i = 0; i < x; i++) {
         matrix[i] = [];
         for (let j = 0; j < y; j++) {
-            matrix[i][j] = 0; 
+            matrix[i][j] = 0;
         }
     }
 
-    console.log(matrix);
+    for (let i = 0; i < x; i++) {
+        for (let j = 0; j < y - i; j++) {
+            if (matrix[i][j] === 0) {
+                matrix[i][j] = currentNumber;
+                currentNumber++;
+            }
+        }
 
+        for (let j = i + 1; j < y - i; j++) {
+            if (matrix[j][x - i - 1] === 0) {
+                matrix[j][x - i - 1] = currentNumber;
+                currentNumber++;
+            }
+        }
+
+        for (let j = y - i - 1; j >= i; j--) {
+            if (matrix[x - i - 1][j] === 0) {
+                matrix[x - i - 1][j] = currentNumber;
+                currentNumber++;
+            }
+        }
+
+        for (let j = y - i - 1; j >= i; j--) {
+            if (matrix[j][i] === 0) {
+                matrix[j][i] = currentNumber;
+                currentNumber++;
+            }
+        }
+    }
+
+    matrix.forEach(line => {
+        console.log(line.join(' '));
+    });
 }
 
-solve(5, 5);
+solve(3, 3);
