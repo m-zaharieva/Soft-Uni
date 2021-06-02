b059d0ae60b264ccb76e763b74892aece997d75b
Мирослава Захариева <miroslava.zaharieva@gmail.com>
2021-06-02 15:54:34 +0300

    edit files

---
+++ JS-Advanced/Objects-and-Composition-Exercise/towns-to-JSON.js
@@ -0,0 +1,12 @@
+function solve(arr) {
+    let pathern = /([A-Za-z]+)/g;
+    
+
+
+}
+
+solve([
+    '| Town | Latitude | Longitude |',
+    '| Sofia | 42.696552 | 23.32601 |',
+    '| Beijing | 39.913818 | 116.363625 |'
+]);
