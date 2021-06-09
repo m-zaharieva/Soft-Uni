47b7ebac16569bb677f52b2cb9c6c3ade82a249e
Мирослава Захариева <miroslava.zaharieva@gmail.com>
2021-06-09 13:42:58 +0300

    edit file

--- JS-Advanced/DOM-Introduction-Exercise/07. Hell's Kitchen/app.js
+++ JS-Advanced/DOM-Introduction-Exercise/07. Hell's Kitchen/app.js
@@ -17,36 +17,55 @@ function solve() {
 
          // the case when the name of the restaurant is not present in the object. 
          if (!restaurantsCatalogue.hasOwnProperty(name)) {
-            // create subObjectg, where tyhe key is the restaurant name, and te value is an object, including employees, and salaries
+            // create subObjectg, where tyhe key is the restaurant name, 
+            // and te value is an object, including employees, and salaries
             restaurantsCatalogue[name] = {
                employees: {},
                avarageSallery: 0,
                heiestSallery: 0
             }
-            // filling the object, which is a value to employees' key
-            for (let i = 0; i < employeesArr.length; i+=2) {
-               restaurantsCatalogue[name].employees[employeesArr[i]] = Number(employeesArr[i+1]);
-            }
+         }  
+           
+         // filling the object, which is a value to employees' key
+         for (let i = 0; i < employeesArr.length; i+=1) {
+            let [employeeName, employeeSallery] = employeesArr[i].split(' ');
+            restaurantsCatalogue[name].employees[employeeName] = Number(employeeSallery);
+         }
 
-            // finding the avarage sallery
-            let sumOfSalleries = Object
-               .values(restaurantsCatalogue[name].employees)
-               .reduce((acc, x) => {return acc + x}, 0);
-
-            restaurantsCatalogue[name].avarageSallery = 
+         // finding the avarage sallery
+         let sumOfSalleries = Object
+            .values(restaurantsCatalogue[name].employees)
+            .reduce((acc, x) => {return acc + x}, 0);
+         restaurantsCatalogue[name].avarageSallery = 
             sumOfSalleries / (Object.values(restaurantsCatalogue[name].employees)).length;
 
-            // finding the highest sallery
-            restaurantsCatalogue[name].heiestSallery = Math.max(...Object.values(restaurantsCatalogue[name].employees));
-
-         } else {
-
-         }
+         // finding the highest sallery
+         restaurantsCatalogue[name].heiestSallery = 
+            Math.max(...Object.values(restaurantsCatalogue[name].employees));
       }
 
+      // finding the best restaurant
+      let sortedRestaurants = Object.keys(restaurantsCatalogue).sort((a,b) => {
+         let restaurantAvarageSaleryA = restaurantsCatalogue[a].avarageSallery
+         let restaurantAvarageSaleryB = restaurantsCatalogue[b].avarageSallery
+         return restaurantAvarageSaleryB - restaurantAvarageSaleryA;
+      })
+
+      // display the results
+      let bestRestaurantName = sortedRestaurants[0];
+      let bestRestaurantElement = document.querySelector('#bestRestaurant p');
+      bestRestaurantElement.textContent = 
+         `Name: ${bestRestaurantName} Average Salary: ${(restaurantsCatalogue[bestRestaurantName].avarageSallery).toFixed(2)} Best Salary: ${(restaurantsCatalogue[bestRestaurantName].heiestSallery).toFixed(2)}`
       
-      // 3. Calculate avarage sallery and highest sallery 
-      
+      let bestEmployeesElement = document.querySelector('#workers p');
+      let result = [];
+      Object.keys(restaurantsCatalogue[bestRestaurantName].employees).forEach(el => {
+         let name = el;
+         let sallery = restaurantsCatalogue[bestRestaurantName].employees[name];
+         result.push(`Name: ${name} With Salary: ${sallery}`);
+      })
+
+      bestEmployeesElement.textContent = result.join(' ');
    }
 }
 
